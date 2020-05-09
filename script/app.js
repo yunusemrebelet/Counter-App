9d5251ee22c26dc75ac73970b090e8f6f9405fc0
Yunus Emre Belet <yunusemrebelet@gmail.com>
2020-05-09 23:57:41 +0300

    second-commit

--- script/app.js
+++ script/app.js
@@ -1,7 +1,7 @@
 var integer_number = 0;
 function decrease() {
   let number_decrease = document.getElementsByTagName("h2").item(0);
-  number_decrease.innerHTML = integer_number--;
+  number_decrease.innerHTML = --integer_number;
 }
 function reset() {
   let number_reset = document.getElementsByTagName("h2").item(0);
@@ -10,5 +10,5 @@ function reset() {
 }
 function increase() {
   let number_increase = document.getElementsByTagName("h2").item(0);
-  number_increase.innerHTML = integer_number++;
+  number_increase.innerHTML = ++integer_number;
 }
